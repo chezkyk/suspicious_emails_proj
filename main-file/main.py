1c36ddccc5cb1a36cdf17b29cc6a76a33f0e1c4c
chezkyk <Y0548532123@GMAIL.COM>
2024-11-14 17:43:55 +0200

    added a init call to initiation the postgres tables with default values (I had a lot of errors because it didn't recognize the tables).

--- main-file/main.py
+++ main-file/main.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 from blueprints.user_blueprint import user_bp
 from blueprints.init_blueprint import init_bp
+from inition import init
 
 app = Flask(__name__)
-
+init()
 app.register_blueprint(init_bp)
 app.register_blueprint(user_bp)
 
