a2da47ac0aec33485e517d7e549b8e31ce4025a6
chezkyk <Y0548532123@GMAIL.COM>
2024-11-14 16:58:37 +0200

    added explosive and hostage consumer files.

---
+++ explosiv-messages-consumer/explosiv_messages_consumer.py
@@ -0,0 +1,20 @@
+import json
+from kafka import KafkaConsumer
+
+from service.consumer_service import insert_message
+
+
+consumer = KafkaConsumer(
+    'messages.explosive',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    group_id='email_messages',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+
+for msg in consumer:
+    message = msg.value
+    print(f"Received message: {message}")
+    insert_message(message)
+    print("Message inserted")
